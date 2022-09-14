2966cb78604e3dcd7db9d44d8103ad232813be9d
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-09-14 17:39:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
 
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function draw() {
+    background("#666464");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
+}
