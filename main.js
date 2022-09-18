f87e66f122d493c22725d73f4ee4a2651d659f3b
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-09-18 17:31:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
+leftWristX = 0;
+rightWrist = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
-    video.size(550, 500);
+    video.size(550, 550);
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded() {
-    console.log("Model is Loaded");
-}
-
 function draw() {
     background("#666464");
+    textSize(difference);
+    fill("#94ecf7");
+    text('Arya', 100, 100);
+}
+
+function modelLoaded() {
+    console.log("Model Has Loaded");
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
